4ba91cc6d6f4a52e7ff9e8fb3451b8b0eca412f2
h.matsuo <hiroshi_matsuo@ec-cube.co.jp>
2021-11-11 19:29:55 +0900

    init

---
+++ app.js
@@ -0,0 +1,30 @@
+const { Octokit } = require("@octokit/rest")
+const octokit = new Octokit()
+const dayjs = require("dayjs")
+
+const START_DATE = new dayjs('2021/09/28')
+
+octokit
+    .paginate('GET /repos/ec-cube/ec-cube/pulls', {
+        state: 'closed',
+        sort: 'updated',
+        direction: 'updated',
+        per_page: 100
+    }, (response, done) => {
+        const data = response.data.filter(pull => {
+            if (pull.merged_at === null) {
+                return false
+            }
+            return dayjs(pull.merged_at).isAfter(START_DATE)
+        })
+
+        done() // todo とりあえず1ページで処理を終えとく
+        return data
+    })
+    .then(pulls => {
+        pulls.map(pull =>
+            console.log(`#${pull.number}: ${pull.merged_at} ${pull.user.login}: ${pull.title}`)
+        )
+        console.log(pulls.length)
+    })
+    .catch(e => console.error(e))
